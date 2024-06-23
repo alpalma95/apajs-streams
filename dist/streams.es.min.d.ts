26ce29b03f37b9c63d12bb7a44f8ccf1d019ab23
Al <alpalma95@gmail.com>
2024-06-23 19:18:49 +0200

    Fix type declaration

--- dist/streams.es.min.d.ts
+++ dist/streams.es.min.d.ts
@@ -1,10 +1,13 @@
-class Effect {
-    private cb: () => void;
-    private _set: Set<Effect>;
-    constructor(cb: () => void);
-    public unhook(): void;
+declare module '@reactivjs/streams' {
+    class Effect {
+       private cb: () => void;
+       private _set: Set<Effect>;
+       constructor(cb: () => void);
+       public unhook(): void;
+   }
+   
+    const hook: (cb: () => void) => Effect;
+   
+    const stream: <T>(initialVal: any) => T & { val: T };
 }
 
-declare const hook: (cb: () => void) => Effect;
-
-declare const stream: <T>(initialVal: any) => T & { val?: T };
