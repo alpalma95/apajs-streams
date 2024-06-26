734f8c1482688cd3263a3e34875c7ddecc9526ae
Al <alpalma95@gmail.com>
2024-06-26 12:31:32 +0200

    Export types instead of declaring module

--- src/stream.d.ts
+++ src/stream.d.ts
@@ -1,13 +1,12 @@
-declare module '@reactivjs/streams' {
-    class Effect {
-       private cb: () => void;
-       private _set: Set<Effect>;
-       constructor(cb: () => void);
-       public unhook(): void;
-   }
-   
-    const hook: (cb: () => void) => Effect;
-   
-    const stream: <T>(initialVal: any) => T extends object ? T : { val: T };
+export class Effect {
+    private cb: () => void;
+    private _set: Set<Effect>;
+    constructor(cb: () => void);
+    public unhook(): void;
 }
+   
+export const hook: (cb: () => void) => Effect;
+   
+export const stream: <T>(initialVal: T) => T extends object ? T : { val: T };
+
 
